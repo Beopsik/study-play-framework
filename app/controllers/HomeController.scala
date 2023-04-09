b6c9692622828ba1d908f9ddd8a57c0c5d649e23
Beopsik <mbsik6082@naver.com>
2023-04-09 13:48:43 +0900

    Add GET /hello

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -29,5 +29,8 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   def tutorial() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.tutorial())
   }
-  
+
+  def hello = Action {
+    Ok(views.html.hello())
+  }
 }
