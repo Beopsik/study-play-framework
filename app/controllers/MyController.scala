73fb3ed2d94ef956af1bd88abd5b0ddfaee9e031
Beopsik <mbsik6082@naver.com>
2023-04-09 16:26:25 +0900

    Study Actions, Controllers and Results

--- app/controllers/MyController.scala
+++ app/controllers/MyController.scala
@@ -1,17 +1,51 @@
 package controllers
 
+import akka.util.ByteString
 import configs.AppConfig
 import play.api._
+import play.api.http.HttpEntity
 import play.api.mvc._
 
 import javax.inject.Inject
 
-class MyController @Inject() (config: Configuration, c: ControllerComponents) extends AbstractController (c){
-  def description = Action{
+class MyController @Inject()(config: Configuration, c: ControllerComponents) extends AbstractController(c) {
+  def description = Action {
     Ok(config.get[String]("description"))
   }
 
-  def appConfig=Action{
-    Ok(config.get[AppConfig]("app.config").toString)
+  def appConfig = Action {
+    Result(
+      header = ResponseHeader(200, Map.empty),
+      body = HttpEntity.Strict(ByteString(config.get[AppConfig]("app.config").toString), Some("application/json"))
+    )
+  }
+
+  def request = Action {
+    request =>
+      Ok("Got request [" + request + "]")
+  }
+
+  def implicitRequest = Action {
+    implicit request =>
+      anotherMethod("test implicit keyword")
+      Ok("Got request [" + request + "]")
+  }
+  def anotherMethod(p: String)(implicit request: Request[_]) = {
+    println("====================================================")
+    println("====================================================")
+    println("Implicit request: "+request)
+    println("====================================================")
+    println("====================================================")
+  }
+
+  def simpleResult=Action{
+    Result(
+      header = ResponseHeader(200, Map.empty),
+      body = HttpEntity.Strict(ByteString("simple results"), Some("text/plain"))
+    )
+  }
+
+  def redirect = Action{
+    Redirect("/tutorial")
   }
 }
