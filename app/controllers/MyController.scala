495be3cc441db195438d48b52be6ac70615bbe87
Beopsik <mbsik6082@naver.com>
2023-04-09 17:15:41 +0900

    Study Body Parser

--- app/controllers/MyController.scala
+++ app/controllers/MyController.scala
@@ -4,6 +4,7 @@ import akka.util.ByteString
 import configs.AppConfig
 import play.api._
 import play.api.http.HttpEntity
+import play.api.libs.json.{JsObject, JsValue}
 import play.api.mvc._
 
 import javax.inject.Inject
@@ -30,22 +31,36 @@ class MyController @Inject()(config: Configuration, c: ControllerComponents) ext
       anotherMethod("test implicit keyword")
       Ok("Got request [" + request + "]")
   }
+
   def anotherMethod(p: String)(implicit request: Request[_]) = {
     println("====================================================")
     println("====================================================")
-    println("Implicit request: "+request)
+    println("Implicit request: " + request)
     println("====================================================")
     println("====================================================")
   }
 
-  def simpleResult=Action{
+  def simpleResult = Action {
     Result(
       header = ResponseHeader(200, Map.empty),
       body = HttpEntity.Strict(ByteString("simple results"), Some("text/plain"))
     )
   }
 
-  def redirect = Action{
+  def redirect = Action {
     Redirect("/tutorial")
+  }
+
+  def save = Action { request: Request[AnyContent] =>
+    val body: AnyContent = request.body
+    val jsonBody: Option[JsValue] = body.asJson
+
+    jsonBody
+      .map{json=>
+        Ok("Got: "+(json \ "player").as[JsObject])
+      }
+      .getOrElse(
+        BadRequest("Expecting application/json request body")
+      )
   }
 }
